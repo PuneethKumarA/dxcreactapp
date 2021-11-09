b4b3bdb17c2c66d4ed1b3fa4aa0b91bd55aba74b
PuneethKumarA <apuneethk@gmail.com>
2021-11-09 01:39:20 -0800

    passing properties to components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+
 
 
 function App() {
   return (
     <div className="App">
-    <Greet/>
-    </div>
+    <Greet name = "Babu" college = "IIT">
+      <p>this is children property</p>
+    </Greet>
+    <Greet name = "Lohith" college = "Boston"/>
+    <Greet name = "Karan" college = "Harward"/>
+
+    <Welcome/>
+
+    </div>    
+    
   );
 }
 
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,8 +1,29 @@
 import React from 'react'
+/* function Greet(){
+    return  <h1>welcome abdul</h1>
+} */
 
-function Greet() {
-    return <h1>Welcome Puneeth</h1>
-    
+const Greet = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>hello {props.name} you are from {props.college}</h1>
+            {props.children}
+        </div>
+    )
 }
+//console.log(props);
+/*//const Greet = ({name,college}) => {
+    const Greet = props => {
+        const {name,college} = props
+    //console.log(props);
+    //props.name = 'dxc'
+    return (
+        <div>
+            
+            <h1> hello  {name} you are from {college}</h1>
+        </div>
+    )
+    }*/
 
 export default Greet
