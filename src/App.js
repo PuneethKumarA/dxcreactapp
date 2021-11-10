83d262788aa70c94ae7427c22cb63e68c7fe88b2
PuneethKumarA <apuneethk@gmail.com>
2021-11-09 23:23:18 -0800

    event binding

--- src/App.js
+++ src/App.js
@@ -6,29 +6,15 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import ClassClick from './components/ClassClick';
 import FunctionClick from './components/FunctionClick';
+import EventBind from './components/EventBind';
 
 
 
 function App() {
   return (
     <div className="App">
-      <ClassClick/>
-      <FunctionClick/>
-      <Counter/>
-      <Message/>
-
-      <Greet name="Babu" college="IIT">
-        <p>this is children property</p>
-      </Greet>
-
-      <Greet name="Lohith" college="Boston">
-        <button>click me</button>
-      </Greet>
-
-      <Greet name="Karan" college="Harward" />
-
-      <Welcome name="Puneeth" alias="puni" />
-
+      <EventBind/>
+      
     </div>
 
   );
