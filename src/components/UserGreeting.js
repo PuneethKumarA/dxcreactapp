148d2692f4d04701586708619660a41ec9003e6c
PuneethKumarA <apuneethk@gmail.com>
2021-11-10 02:58:01 -0800

    conditional rendering using short circuit operator

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -5,17 +5,18 @@ export class UserGreeting extends Component {
         super(props)
 
         this.state = {
-            isLoggedIn: false
+            isLoggedIn: true
         }
     }
 
     render() {
+        return this.state.isLoggedIn && <div>welcome ansari</div>
         
-        return(
+        /* return(
             this.state.isLoggedIn ?
             <div>welcome abdul</div> :
             <div>welcome guest</div>
-        )
+        ) */
 
        /*  let message 
         /* if (this.state.isLoggedIn) {
