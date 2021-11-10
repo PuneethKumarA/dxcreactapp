4dbb096b9d7351a0caf57f9c59b3bda91ea1b7ab
PuneethKumarA <apuneethk@gmail.com>
2021-11-10 02:48:05 -0800

    conditional rendering using variable message

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -5,23 +5,32 @@ export class UserGreeting extends Component {
         super(props)
 
         this.state = {
-             isLoggedIn : true
+            isLoggedIn: true
         }
     }
 
     render() {
-        if(this.state.isLoggedIn){
-            return <div>welcome puneeth</div>
+        let message
+        if (this.state.isLoggedIn) {
+            message = <div>welcome abdul</div>
         }
-        else{
-            return <div>welcome guest</div>
+        else {
+            message = <div>welcome guest</div>
         }
-       /*  return (
-            <div>
-                <div>welcome abdul</div>
-                <div>welcome guest</div>
-            </div>
-        ) */
+        return <div>{message}</div>
+        /*  if(this.state.isLoggedIn){
+             if(this.state.isLoggedIn){
+                 return <div>welcome abdul</div>
+             }
+             else{
+                 return <div>welcome guest</div>
+             }*/
+        /*  return (
+             <div>
+                 <div>welcome abdul</div>
+                 <div>welcome guest</div>
+             </div>
+         ) */
     }
 }
 
