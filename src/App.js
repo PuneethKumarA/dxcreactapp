5141d71c2c19f9cd6803693ecd43979f554e00d4
PuneethKumarA <apuneethk@gmail.com>
2021-11-12 01:25:24 -0800

    refs demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import ClassClick from './components/ClassClick';
-import FunctionClick from './components/FunctionClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Table from './components/Table';
-import Stylesheet from './components/StyleSheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import Fragments from './components/Fragments';
-import NewTable from './components/NewTable';
-import PureComp from './components/PureComp';
-import Parentcomp from './components/ParentComp';
-import Form from './components/Form';
+
+import RefsDemo from './components/RefsDemo';
 
 
 
@@ -27,7 +9,7 @@ import Form from './components/Form';
 function App() {
   return (
     <div className="App">
-      <Parentcomp />
+      <RefsDemo/>
 
     </div>
 
