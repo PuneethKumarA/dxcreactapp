10b21c5a2c677c6835c1441d53e5b3481467d38d
PuneethKumarA <apuneethk@gmail.com>
2021-11-16 21:43:08 -0800

    portal demo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RefsDemo from './components/refs/RefsDemo';
 import FocusInput from './components/refs/FocusInput';
 import Input from './components/refs/Input';
 import FrParentInput from './components/refs/FrParentInput';
+import PortalDemo from './components/portals/PortalDemo';
 
 
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <FrParentInput/>
+      <PortalDemo/>
     </div>
 
   );
