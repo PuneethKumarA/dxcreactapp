a6fdd80d53d47c00e792f72a4181df84a8083b31
PuneethKumarA <apuneethk@gmail.com>
2021-11-17 01:44:08 -0800

    use effect hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-import HookCounter3 from './components/hooks/HookCounter3';
+import CounterHook1 from './components/hooks/useeffect/CounterHook1';
+import ClassCounter1 from './components/hooks/useeffect/ClassCounter1';
 
 
 
@@ -12,8 +13,8 @@ import HookCounter3 from './components/hooks/HookCounter3';
 function App() {
   return (
     <div className="App">
-     
-      <HookCounter3/>
+      
+      <CounterHook1/>
     </div>
 
   );
